07cc70438d43ef572f0fa818aa9b9124d88c43a2
clshu <clshu2020@gmail.com>
2022-10-06 17:04:40 +0800

    Lecture #12.2: Client Side Implementation: 1. Create asyncStub in LaptopClient:   private final LaptopServiceStub asyncStub; 2. Create cleint method:   LaptopClient.uploadImage(String laptopID, String imagePath);. 3. Initialize StreamObserver<UploadImageRequest> requestObserver with: - Implement  StreamObserver<UploadImageResponse> - onNext(), oneError() and onMethod 4. Call requestObserver methods: - Call onNext to send ImageInfo first - Then loop and read image from the fil, call onNext to send chuck data until no data to read. - Call onComplete to finish it 5. Use CountDownLatch to synchronize.

--- src/main/java/com/techschool/pcbook/service/LaptopClient.java
+++ src/main/java/com/techschool/pcbook/service/LaptopClient.java
@@ -1,14 +1,20 @@
 package com.techschool.pcbook.service;
 
+import com.google.protobuf.ByteString;
 import com.techschool.pcbook.pb.*;
-import com.techschool.pcbook.pb.LaptopServiceGrpc.LaptopServiceBlockingStub ;
+import com.techschool.pcbook.pb.LaptopServiceGrpc.LaptopServiceBlockingStub;
+import com.techschool.pcbook.pb.LaptopServiceGrpc.LaptopServiceStub;
 import com.techschool.pcbook.sample.Generator;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +24,7 @@ public class LaptopClient {
 
     private final ManagedChannel channel;
     private final LaptopServiceBlockingStub blockingStub;
+    private final LaptopServiceStub asyncStub;
 
     public LaptopClient(String host, int port) {
         channel = ManagedChannelBuilder.forAddress(host, port)
@@ -25,6 +32,7 @@ public class LaptopClient {
                 .build();
 
         blockingStub = LaptopServiceGrpc.newBlockingStub(channel);
+        asyncStub = LaptopServiceGrpc.newStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
@@ -79,30 +87,107 @@ public class LaptopClient {
         // May log more info later
     }
 
+    public void uploadImage(String laptopID, String imagePath) {
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+
+        StreamObserver<UploadImageRequest> requestObserver =
+                asyncStub
+                .withDeadlineAfter(5, TimeUnit.SECONDS)
+                .uploadImage(new StreamObserver<UploadImageResponse>() {
+            @Override
+            public void onNext(UploadImageResponse response) {
+                logger.info("received response:\n" + response);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.log(Level.SEVERE, "upload failed: " + t);
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("image uploaded.");
+                finishLatch.countDown();
+            }
+        });
+
+        FileInputStream fileInputStream;
+
+        try {
+            fileInputStream = new FileInputStream(imagePath);
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "cannot find image file: " + imagePath);
+            return;
+        }
+
+        String imageType = imagePath.substring(imagePath.lastIndexOf("."));
+        ImageInfo info = ImageInfo.newBuilder().setLaptopId(laptopID).setImageType(imageType).build();
+        UploadImageRequest request = UploadImageRequest.newBuilder().setInfo(info).build();
+
+        try {
+            requestObserver.onNext(request);
+            logger.info("sent image info:\n" + info);
+
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int n = fileInputStream.read(buffer);
+                if (n <= 0) {
+                    break;
+                }
+                if (finishLatch.getCount() == 0) {
+                    return;
+                }
+
+                request = UploadImageRequest.newBuilder().setChunkData(ByteString.copyFrom(buffer, 0, n)).build();
+                requestObserver.onNext(request);
+                logger.info("sent chunk data size: " + n);
+            }
+
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "unexpected error: " + e.getMessage());
+            requestObserver.onError(e);
+            return; // ??
+        }
+
+        requestObserver.onCompleted();
+
+        try {
+            if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+                logger.warning(("request cannot finish within 1 minute"));
+            }
+        } catch (InterruptedException e) {
+            logger.info("request is interrupted: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         LaptopClient client = new LaptopClient("localhost", 50051);
 
         Generator generator = new Generator();
 
         try {
-            for (int i = 0; i < 10; i++) {
-                Laptop laptop = generator.NewLaptop();
-                client.createLaptop(laptop);
-            }
-
-            Memory minRam = Memory.newBuilder()
-                    .setValue(8)
-                    .setUnit(Memory.Unit.GIGABYTE)
-                    .build();
-
-            LaptopFilter filter = LaptopFilter.newBuilder()
-                    .setMaxPriceUsd(3000)
-                    .setMinCpuCores(4)
-                    .setMinCpuGhz(2.5)
-                    .setMinRam(minRam)
-                    .build();
-
-            client.SearchLaptop(filter);
+//            for (int i = 0; i < 10; i++) {
+//                Laptop laptop = generator.NewLaptop();
+//                client.createLaptop(laptop);
+//            }
+//
+//            Memory minRam = Memory.newBuilder()
+//                    .setValue(8)
+//                    .setUnit(Memory.Unit.GIGABYTE)
+//                    .build();
+//
+//            LaptopFilter filter = LaptopFilter.newBuilder()
+//                    .setMaxPriceUsd(3000)
+//                    .setMinCpuCores(4)
+//                    .setMinCpuGhz(2.5)
+//                    .setMinRam(minRam)
+//                    .build();
+//
+//            client.SearchLaptop(filter);
+            Laptop laptop = generator.NewLaptop();
+            client.createLaptop(laptop);
+            client.uploadImage(laptop.getId(), "tmp/macbook-air-gold-2015-16.jpg");
         } finally {
             try {
                 client.shutdown();
